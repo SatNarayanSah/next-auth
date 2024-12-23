f6c7fe35c6264d8552692fce837bca9b28b9ffa4
Satyam Sah <satnarayan5166@gmail.com>
2024-12-24 01:19:00 +0545

    mailer setup

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,26 +1,42 @@
-import {connect} from '@/dbConfig/dbConfib'
+import { connect } from '@/dbConfig/dbConfib'
 import User from '@/models/userModel'
-import {NextRequest, NextResponse} from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import bcryptjs from 'bcryptjs'
+import { sendEmail } from '@/helper/Mailer'
 
 connect()
 
-export async function POST(request: NextRequest){
+export async function POST(request: NextRequest) {
     try {
         const reqBody = request.json()
-        const {username, email, password} = reqBody
+        const { username, email, password } = reqBody
         //validatin
         console.log(reqBody);
-        
-        await User.findone({email})
-        if(user){
-            return NextResponse.json({error: "User already exists"}, {status: 400})
+
+        await User.findone({ email })
+        if (user) {
+            return NextResponse.json({ error: "User already exists" }, { status: 400 })
         }
 
         const salt = await bcryptjs.genSalt(10);
-        const hashedPassword
+        const hashedPassword = await bcryptjs.hash(password, salt)
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+        const savedUser = await newUser.save()
+        console.log(savedUser);
+
+        //send  varification email
+        await sendEmail({email, emailType: "VERIFY", userId:savedUser._id})
+        return NextResponse.json({
+            message: "User Register Successfully",
+            success: true,
+            savedUser
+        })
 
     } catch (error: any) {
-        return NextResponse.json({error: error.message}, {status: 500})
+        return NextResponse.json({ error: error.message }, { status: 500 })
     }
 }
--- src/helper/Mailer.ts
+++ src/helper/Mailer.ts
@@ -1,11 +1,20 @@
+import User from '@/models/userModel';
 import nodemailer from 'nodemailer'
+import bcryptjs from 'bcryptjs'
+
+
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
+    //TODO user configurations
     try {
+        await bcryptjs
+        if (emailType === "VERIFY") {
+            await User.findByIdAndUpdate(userId,
+                { verifyToken }
+            )
 
-        //TODO user configurations
+        }
 
-        
         const transporter = nodemailer.createTransport({
             host: "smtp.ethereal.email",
             port: 587,
